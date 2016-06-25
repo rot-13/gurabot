3633ec1b495e43f8737ee468e3753494fa309a18
Shay Davidson <sdavidson@paypal.com>
2016-06-25 19:50:18 +0300

    Configured basics of sinatra app

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+ruby '2.3.0'
+
+gem 'sinatra', '1.4.7'
+gem 'rumba', '0.2.6'
+gem 'colorize', '0.7.7'
---
+++ server/index.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+require 'rumba'
+require 'colorize'
+
+set :root, '../'
+set :public_folder, 'public'
+set :roomba_port, '/dev/ttyUSB0'
+
+begin
+	roomba = Roomba.new(settings.roomba_port)
+rescue Exception => e
+	puts "Error connecting to Roomba (reason: #{e}).".colorize(:red)
+end
+
+get '/' do
+	File.read(File.join(settings.public_folder, 'index.html'))
+end
