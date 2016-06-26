bde8ec9fa5a962a029da790f12e6823efe0b5c0b
Gilad Goldberg <ggoldberg@paypal.com>
2016-06-26 10:43:12 +0300

    rbenv + capistrano

--- Gemfile
+++ Gemfile
@@ -6,3 +6,4 @@ gem 'rumba', '0.2.6'
 gem 'colorize', '0.7.7'
 gem 'capistrano'
 gem "capistrano-bundler"
+gem 'capistrano-rbenv', '~> 2.0'
--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,16 @@ set :repo_url, 'git@github.com:rot-13/gurabot.git'
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/home/pi/gurabot'
 
+
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_ruby, File.read('.ruby-version').strip
+
+
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
+
+
 # Default value for :scm is :git
 # set :scm, :git
 
