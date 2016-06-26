b681a2723f3eea8aa6ab84759a7a1c6a7ddd9614
Benny Weingarten-Gabbay <gardenofwine@gmail.com>
2016-06-26 17:30:27 +0300

    Add Wrecking ball sound

--- server/roomba_api.rb
+++ server/roomba_api.rb
@@ -59,6 +59,13 @@ module RoombaApi
 		end
 	end
 
+	def wrecking_ball(roomba) 
+		command(roomba) do
+			sound(roomba,3,[[70, 1],[70, 1],[70, 1],[70, 1],[70, 1],[70, 3],[69, 1],[69, 7], [65, 1], [70, 1], [69, 1], [67, 1], [65, 1], [70, 3],[69, 1],[69, 4]])
+			roomba.play_sound(3)
+			# ser.write('\x8c\x03\x10F\x10F\x10F\x10F\x10F\x10F\x30E\x10E\x70A\x10F\x10E\x10C\x10A\x10F\x30E\x10E\x40')
+		end
+	end
 	######################
 
 	def command(roomba)
@@ -74,4 +81,18 @@ module RoombaApi
 		return MODES[:passive]
 	end
 
+	# A fixed version of Roomba's song function
+	def song(roomba, song_number, notes)
+	    raise RangeError if song_number < 0 || song_number > 15
+	    
+	    notes.map! do |i|
+	      note, duration = i
+
+	      # notes can either be a string or the actual ID
+	      note = Roomba::NOTES[note] if note.is_a?(String)
+	      [note, duration*16]
+	    end
+
+	    roomba.write_chars([Roomba::SONG, song_number, notes.size] + notes.flatten)
+	end
 end
