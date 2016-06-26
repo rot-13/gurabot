3d74dba9b33d7de6378fd5dfe3cf470e7513f2f3
Shay Davidson <sdavidson@paypal.com>
2016-06-26 13:48:03 +0300

    Added battery

--- client/index.js
+++ client/index.js
@@ -3,12 +3,27 @@ require('index.scss')
 const $ = require('jquery')
 
 function sendCommand(type, data) {
-	$.post(`/command/${type}`, data).fail(() => { alert('whoops, something went wrong') })
+	$.post(`/command/${type}`, data)
+		.fail(() => { alert('whoops, something went wrong') })
+}
+
+function getInfo(type, callback) {
+	$.get(`/info/${type}`)
+		.done(callback)
+		.fail(() => { alert('whoops, something went wrong') })
 }
 
 function sendCommandHandler() {
 	sendCommand(this.id)
 }
 
+function setBattery(val) {
+	$('.battery').removeClass('fa-battery-4 fa-battery-3 fa-battery-2 fa-battery-1 fa-battery-0')
+	let classVal = Math.round(val * 4)
+	$('.battery').addClass(`fa-battery-${classVal}`)
+}
+
 $('#move_forward').click(sendCommandHandler)
 $('#move_backward').click(sendCommandHandler)
+
+setBattery(1) // TODO: fetch periodically.
