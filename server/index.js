a99e6a97b6651f30e029b4f1790442c2c1ede8f9
Shay Davidson <sdavidson@paypal.com>
2016-06-28 14:37:06 +0300

    bla

--- server/index.js
+++ server/index.js
@@ -1,4 +1,27 @@
-// TODO: PM2 deployment
+const koa = require('koa')
+const route = require('koa-route')
+const serve = require('koa-static')
+const webpack = require('webpack')
+
+const PORT = 4567
+const app = koa()
+
+if (process.env !== 'PRODUCTION') {
+	const webpackMiddleware = require('koa-webpack-dev-middleware')
+	const config = require('../webpack.config')
+	config.bail = false
+	app.use(webpackMiddleware(webpack(config)))
+}
+
+app.use(serve('./public'))
+
+app.listen(PORT)
+console.log(`GuraBot listening on port ${PORT}`);
+
+// app.use(serve);
+
+// TODO: KOA
+// TODO: PM2 deployment (set NODE_ENV=production)
 // TODO: serviceworkers caching
 // TODO: webpack middleware
 // TODO: webpack html for caching + hash
