bfe83d1223d897f7cde75a06cac1c12e46d7bbb8
Shay Davidson <sdavidson@paypal.com>
2016-07-07 08:23:31 +0300

    cap tests

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -9,14 +9,3 @@
 server "roomba.local", :user => "pi", :roles => %{web app}, ssh_options: { password: 'raspberry' }
 set :branch, "master"
 set :ssh_options, { :forward_agent => true }
-
-after :deploy, "deploy:restart_server"
-
-namespace :deploy do
-
-	task :restart_server do
-		on primary roles :app do
-			run "fuser -k 4567/tcp && cd '#{current_path}'; bundle exec ruby ./index.rb"
-		end
-	end
-end
