82d6115257f2ea4b3c8f936c778710bfe4cc3c00
Shay Davidson <sdavidson@paypal.com>
2016-07-12 08:35:44 +0300

    More cap

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,12 +1,4 @@
-# server-based syntax
-# ======================
-# Defines a single server with a list of roles and multiple properties.
-# You can define all roles on a single server, or split them:
-
-# server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
-# server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
-# server 'db.example.com', user: 'deploy', roles: %w{db}
-server "roomba.local", :user => "pi", :roles => %{web app}, ssh_options: { password: 'raspberry' }
+server "roomba.local", :user => "pi", :roles => %{web app}, ssh_options: { password: "raspberry" }
 set :branch, "master"
 set :ssh_options, { :forward_agent => true }
 
@@ -16,7 +8,7 @@ task :restart_server do
 	on release_roles :all do
 		within "#{release_path}" do
 			execute("fuser -k 4567/tcp", raise_on_non_zero_exit: false)
-			execute(:nohup, :bundle, :exec, :ruby, './index.rb', '>/dev/null', '2>&1', '&' pty: false)
+			execute(:nohup, :bundle, :exec, :ruby, "./index.rb", ">/dev/null", "2>&1", "&")
 		end
 	end
 end
