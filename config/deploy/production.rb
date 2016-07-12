a07c972adc38b83c93ab88724f2a4a0b0a43d72d
Shay Davidson <sdavidson@paypal.com>
2016-07-12 08:41:07 +0300

    Cap...

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,9 +6,7 @@ after :deploy, :restart_server
 
 task :restart_server do
 	on release_roles :all do
-		within "#{release_path}" do
-			execute("fuser -k 4567/tcp", raise_on_non_zero_exit: false)
-			run("nohup bundle exec ruby ./index.rb >/dev/null 2>&1 &", pty: false)
-		end
+		execute("fuser -k 4567/tcp", raise_on_non_zero_exit: false)
+		execute("cd ~/gurabot/current &&", :nohup, :bundle, :exec, :ruby, "./index.rb", ">/dev/null", "2>&1", "&")
 	end
 end
