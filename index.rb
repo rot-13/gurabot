ef0d458e814b7d1c95dd82aa4e4da5e69ca8fbce
Shay Davidson <sdavidson@paypal.com>
2016-07-31 15:59:28 +0300

    work

--- index.rb
+++ index.rb
@@ -78,7 +78,7 @@ def play_behavior(name)
 	behavior.each do |command|
 		instruction = command.split('_')
 		instruction_name = instruction[0]
-		instruction_prop = instruction[1]
+		instruction_prop = instruction[1].to_i
 
 		case instruction_name
 		when "sfx"
